f7b8e0a36ef83b21d2456b37758f8cae30f8b15a
ShuhratBek <Shukhratbek_Mamadaliev@epam.com>
2016-06-23 18:42:15 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+/// <reference path="../typings/index.d.ts"/>
+
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import './index.scss';
+
+import { AppComponent } from './app';
+
+import { enableProdMode } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import '@angular2-material/core';
+
+declare var process: any;
+if (process.env.NODE_ENV === 'production') {
+    enableProdMode();
+}
+
+bootstrap( AppComponent, [
+    HTTP_PROVIDERS
+]);
