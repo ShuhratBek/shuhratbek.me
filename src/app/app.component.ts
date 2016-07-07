251205e25c9b4712cd899da979d97e46df12b2c9
ShuhratBek <Shukhratbek_Mamadaliev@epam.com>
2016-07-07 17:53:44 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from './sidenav';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { MdIconRegistry } from '@angular2-material/icon';
+import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
 import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 
 @Component({
     selector: 'app',
@@ -10,8 +11,24 @@ import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
     directives: [
         SidenavComponent,
         ROUTER_DIRECTIVES,
-        MD_SIDENAV_DIRECTIVES
-    ]
+        MD_SIDENAV_DIRECTIVES,
+        MD_ICON_DIRECTIVES,
+        MD_BUTTON_DIRECTIVES
+    ],
+    styles: [`
+        * {
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+        md-sidenav-layout {
+            background: rgba(0,0,0,0.03);
+        }
+        .sidenav {
+            min-width: 300px;
+            background: rgba(0,0,0,0.7);
+            color: #fff;
+        }
+    `]
 })
 export class AppComponent {
     constructor(private mdIconRegistry: MdIconRegistry) {
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -3,16 +3,21 @@ import { SidenavService } from './sidenav.service';
 import { Menu } from './menu.model';
 import { ROUTER_DIRECTIVES }  from '@angular/router';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
-import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 
 @Component({
     selector: 'sidenav',
     templateUrl: 'app/sidenav/sidenav.component.html',
     directives: [
         MD_BUTTON_DIRECTIVES,
-        MD_SIDENAV_DIRECTIVES,
-        ROUTER_DIRECTIVES
+        ROUTER_DIRECTIVES,
+        MD_LIST_DIRECTIVES
     ],
+    styles: [`
+        .md-list-item {
+            color: #fff;
+        }
+    `],
     providers: [
         SidenavService
     ]
